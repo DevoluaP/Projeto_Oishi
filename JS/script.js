949a0102ed245d6374754929f80848b951bbb860
roluaP <paulohenrique456moreira@gmail.com>
2023-05-14 23:28:20 -0300

    projeto pronto

--- JS/script.js
+++ JS/script.js
@@ -23,3 +23,31 @@ $(document).ready(() => {
         $('.menu-principal').css('display', 'none');
     });
 });
+
+$('.imagens-do-ambiente').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
